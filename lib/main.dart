119d9070986060cdfcd61b79b49cf85e259d62e4
yicheng0626 <410637085@gms.tku.edu.tw>
2022-09-26 15:33:24 +0800

    first commit_0926

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() {
   var appTitle = Text('我的第一個Flutter App'),
-  hiFlutter = Text(
-    'Hi, Flutter.',
-    style: TextStyle(fontSize: 50),
+  hiFlutter = Text( 'Hi, Flutter.\n今天是20220926',
+
+      style: TextStyle(fontSize: 30,
+      decoration: TextDecoration.underline,
+      color: Colors.white
+
+    ),
   );
 
   var appBody = Center(
+
     child: hiFlutter,
+
+
+
   );
 
   var appBar = AppBar(
     title: appTitle,
+    backgroundColor: Color(0xFF004D40),
   );
 
   var app = MaterialApp(
     home: Scaffold(
       appBar: appBar,
       body: appBody,
+      backgroundColor: Color(0xFFB2DFDB),
     ),
   );
 
